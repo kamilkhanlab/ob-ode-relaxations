347e0dddca2a1efa225edd23fcc00040fef34795
Syykkkk <64610560+Syykkkk@users.noreply.github.com>
2021-04-10 20:36:12 -0400

    Add "src" and "test" folders

--- functions/linear_transformation.m
+++
@@ -1,8 +0,0 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%A function that defines the linear transformation "v" in the article.    %
-%                                                                         %
-%Last modified by Yingkai Song 08/18/2020                                 %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function v = linear_transformation(alpha,xicv,xicc)
-v = (alpha+1)./2.*xicc-(alpha-1)./2.*xicv;
-end
